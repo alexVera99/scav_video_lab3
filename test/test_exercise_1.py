396682837c8e391110f2374bfabbb0a48244be74
alexVera99 <alexanderjoel.vera99@gmail.com>
2022-10-26 18:07:35 +0200

    test: test for exercise 1

---
+++ test/test_exercise_1.py
@@ -0,0 +1,36 @@
+import pathlib
+from src.lib import exercise_1 as ex1
+import unittest
+
+
+class TestExercise1(unittest.TestCase):
+    def setup_class(self):
+        self.video_filename = pathlib.Path("../../data/bbb.mp4")
+        self.video_filename_fake = pathlib.Path("/fake/path")
+
+    def test_get_metadata(self):
+        expected_data = {
+            "duration": "00:09:56.48",
+            "bitrate": "829 kb/s",
+            "num_streams": 2,
+            "streams": [
+                {
+                    "name": "Stream #0:0[0x1](und)",
+                    "type": "Video",
+                    "codec": "h264 (Constrained Baseline) (avc1 / 0x31637661)",
+                },
+                {
+                    "name": "Stream #0:1[0x2](und)",
+                    "type": "Audio",
+                    "codec": "aac (LC) (mp4a / 0x6134706D)"
+                }
+            ]
+        }
+
+        metadata = ex1.get_metadata(self.video_filename)
+
+        self.assertDictEqual(metadata, expected_data)
+
+    def test_get_metadata_no_such_a_file(self):
+        with self.assertRaises(Exception):
+            ex1.get_metadata(self.video_filename_fake)
