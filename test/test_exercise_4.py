dddeeecf7527ed21faaed8e54662d3f073577ca8
alexVera99 <alexanderjoel.vera99@gmail.com>
2022-11-03 15:36:46 +0100

    test: add a new test case

--- test/test_exercise_4.py
+++ test/test_exercise_4.py
@@ -7,6 +7,7 @@ class TestExercise4(unittest.TestCase):
     def setup_class(self):
         self.video_filename = pathlib.Path("../data/bbb.mp4")
         self.video_filename_2 = pathlib.Path("../data/bbb_2_audio_codecs.mp4")
+        self.video_filename_3 = pathlib.Path("../data/bbb_audio_ac3.mp4")
         self.video_filename_fake = pathlib.Path("/fake/path")
 
     def test_analyze_broadcasting_standard(self):
@@ -26,6 +27,18 @@ class TestExercise4(unittest.TestCase):
                            "DTMB"])
         analyzer = ex4.BroadcastingAnalyzer()
         broadcasting_standards = analyzer.analyze(self.video_filename_2)
+        broadcasting_standards = sorted(broadcasting_standards)
+
+        self.assertListEqual(expected_result,
+                             broadcasting_standards)
+
+    def test_analyze_broadcasting_standard_3(self):
+        expected_result = sorted(["DVB-T",
+                                  "ATSC",
+                                  "DTMB"])
+
+        analyzer = ex4.BroadcastingAnalyzer()
+        broadcasting_standards = analyzer.analyze(self.video_filename_3)
         broadcasting_standards = sorted(broadcasting_standards)
 
         self.assertListEqual(expected_result,
